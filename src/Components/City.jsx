bec33f7da2ad7453120ec77572e2ff79af2d3ba2
Ranga Komarthi <88224031+komarthiranga@users.noreply.github.com>
2022-09-07 12:28:20 +0530

    one error case is fixed

--- src/Components/City.jsx
+++ src/Components/City.jsx
@@ -1,42 +1,51 @@
-import {
-  FormGroup,
-  TextField,
-} from "@mui/material";
-import React, { useEffect, useState } from "react";
+import { FormGroup, TextField } from "@mui/material";
+import React from "react";
 import Autocomplete from "@mui/material/Autocomplete";
 import { cities } from "../data/city";
 
-const City = ({isLoading, forecastData, cityName}) => {
-
+const City = ({ isLoading, forecastData, cityName }) => {
   const handleSubmit = (e) => {
     e.preventDefault();
   };
 
   const showWeatherHandler = (event, newValue) => {
     isLoading(true);
-    fetchWeatherForcast(newValue);
+    if (newValue) {
+      fetchWeatherForcast(newValue);
+    } else {
+      isLoading(false);
+    }
   };
 
-  const fetchWeatherForcast = async(selectedCity) => {
-    try{
-        const forcastResponseData = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${selectedCity.label}&APPID=d37deb3cd8797e9402bfeeb15ddc404a`);
-        if(!forcastResponseData.ok) {
-            throw new Error('There is some issue while fetching the data');
-        }
-        const forcastResponse = await forcastResponseData.json();
+  const fetchWeatherForcast = async (selectedCity) => {
+    try {
+      const forcastResponseData = await fetch(
+        `https://api.openweathermap.org/data/2.5/forecast?q=${selectedCity.label}&APPID=d37deb3cd8797e9402bfeeb15ddc404a`
+      );
+      if (!forcastResponseData.ok) {
+        throw new Error("There is some issue while fetching the data");
+      }
+      const forcastResponse = await forcastResponseData.json();
 
-        const currentWeatherResponseData = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${selectedCity.label}&APPID=d37deb3cd8797e9402bfeeb15ddc404a`);
-        if(!currentWeatherResponseData.ok) {
-            throw new Error('There is some issue while fetching the data');
-        }
-        const currentWeatherResponse = await currentWeatherResponseData.json();
+      const currentWeatherResponseData = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${selectedCity.label}&APPID=d37deb3cd8797e9402bfeeb15ddc404a`
+      );
+      if (!currentWeatherResponseData.ok) {
+        throw new Error("There is some issue while fetching the data");
+      }
+      const currentWeatherResponse = await currentWeatherResponseData.json();
 
-        isLoading(false);
-        forecastData(forcastResponse.list, currentWeatherResponse, selectedCity.label);
-    } catch{
-        console.log('There is some issue with fetching Data')
+      isLoading(false);
+      forecastData(
+        forcastResponse.list,
+        currentWeatherResponse,
+        selectedCity.label
+      );
+    } catch {
+      console.log("There is some issue with fetching Data");
+      isLoading(false);
     }
-  }
+  };
 
   return (
     <>
